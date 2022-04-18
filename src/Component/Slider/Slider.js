70685dc17580371e4b1e34465adc0d2ff8aedfd5
harshad1004 <harshadntridhyatech@gmail.com>
2022-04-18 09:52:33 +0530

    slider error in url

--- src/Component/Slider/Slider.js
+++ src/Component/Slider/Slider.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 const SliderData = [
   {
     id: "0",
@@ -40,15 +43,34 @@ const SliderData = [
     brand: "On Everything",
     text: "Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.",
     image: "images/arrival-bg.png",
-  }
+  },
 ];
 const Slider = () => {
+  const [sliderData, setSliderData] = useState([]);
+  useEffect(() => {
+    fetchSlide();
+  }, []);
+  const fetchSlide = () => {
+    axios
+      .get("http://localhost:5000/api/slider")
+      .then((data) => {
+        console.log("57", data);
+        setSliderData(data.data.data);
+      })
+      .catch((err) => {
+        console.log(err.response.data.message);
+      });
+  };
+  //  console.log(sliderData, "slider");
+  console.log(sliderData, "65");
   return (
     <>
       <div id="customCarousel1" className="carousel slide" data-ride="carousel">
         <div className="carousel-inner">
-          {SliderData.map((slider, index) => {
-            console.log(index, "index");
+          {sliderData.map((slider, index) => {
+            {
+              console.log(slider, "72");
+            }
             return (
               <div
                 key={index}
@@ -58,18 +80,18 @@ const Slider = () => {
               >
                 <section className="slider_section">
                   <div className="slider_bg_box">
-                    <img src={slider.image} alt="" />
+                    <img src={slider} alt="" />
                   </div>
                   <div className="container ">
                     <div className="row">
                       <div className="col-md-7 col-lg-6 ">
                         <div className="detail-box">
                           <h1>
-                            <span>{slider.sale}</span>
+                            {/* <span>{slider.sale}</span> */}
                             <br />
-                            {slider.brand}
+                            {/* {slider.brand} */}
                           </h1>
-                          <p>{slider.text}</p>
+                          {/* <p>{slider.text}</p> */}
                           <div className="btn-box">
                             <a href="" className="btn1">
                               Shop Now
@@ -83,8 +105,8 @@ const Slider = () => {
               </div>
             );
           })}
-          </div>
-          <div>
+        </div>
+        <div>
           <div className="container">
             <ol className="carousel-indicators">
               <li
